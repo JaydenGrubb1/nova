a7a7deb93875060f56f6b06100ce759cbb6573ef
Jayden Grubb <contact@jaydengrubb.com>
2025-02-10 23:40:10 +1000

    Add NOVA_VERSION_ macro

---
+++ engine/include/nova/version.h
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2025, Jayden Grubb <contact@jaydengrubb.com>
+ * 
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#pragma once
+
+#define NOVA_VERSION_MAJOR 0
+#define NOVA_VERSION_MINOR 1
+#define NOVA_VERSION_PATCH 0
